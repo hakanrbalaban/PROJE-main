36e20054ad28810711adefd6bb8de429bd54819d
hakanrbalaban <hakanrustubalaban1@gmail.com>
2025-11-05 20:03:43 +0300

    hello

---
+++ react/lama/social/src/App.js
@@ -0,0 +1,73 @@
+import LeftBar from "./components/leftBar/leftBar";
+import Navbar from "./components/navbar/Navbar";
+import RightBar from "./components/rightBar/rightBar";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
+
+function App() {
+  const currentUser = true;
+  const Layout = () => {
+    return (
+      <div>
+        <Navbar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{flex: 6}}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+    return children;
+  };
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/lama/social/src/components/rightBar/rightBar.jsx
@@ -0,0 +1,11 @@
+import './rightBar.scss'
+
+const RightBar = () => {
+  return (
+    <div className='rightBar'>
+      <div className="container"></div>
+    </div>
+  )
+}
+
+export default RightBar
